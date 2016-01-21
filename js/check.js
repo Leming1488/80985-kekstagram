c1175ad7eb3618f2317381222ecda38f66417418
pkozelskiy <pkozelskiy@n-core.ru>
2016-01-21 17:00:21 +0300

    [module1-task2] финальный вариант

--- js/check.js
+++ js/check.js
@@ -1,29 +1,34 @@
-"use strict";
-function getMessage(a,b) {
-  switch (typeof(a)) {
-    case "boolean":
+'use strict';
+
+function getMessage(a, b) {
+  switch (typeof a) {
+    case 'boolean':
       if (a) {
-        return  "Переданное GIF-изображение анимировано и содержит" + " " + b + " " + "кадров";
+        return 'Переданное GIF-изображение анимировано и содержит' + ' ' + b + ' ' + 'кадров';
+      } else {
+        return 'Переданное GIF-изображение не анимировано';
       }
-      else
-        return "Переданное GIF-изображение не анимировано";
       break;
-    case  "number":
-      return  "Переданное SVG-изображение содержит" + " " + a + " " + "объектов и" + " " + (b * 4) + " " + "аттрибутов";
+    case 'number':
+      return 'Переданное SVG-изображение содержит' + ' ' + a + ' ' + 'объектов и' + ' ' + (b * 4) + ' ' + 'аттрибутов';
       break;
-    case "object":
-      if (typeof(b) === "object") {
-        return "test";
-      }
-      else {
+    case 'object':
+      if (typeof b === 'object') {
+        var square = 0;
+        var arr = Math.min(a.length, b.length)
+        for (var i = 0; i < arr; i++) {
+          square += a[i] * b[i];
+        }
+        return 'Общая площадь артефактов сжатия:' + ' ' + square + ' ' + 'пикселей';
+      } else {
         var sum = 0;
-        for(var i =0; i < a.length; i++) {
+        for (var i = 0; i < a.length; i++) {
           sum += a[i];
         }
-        return "Количество красных точек во всех строчках изображения:" + " " + sum;
+        return 'Количество красных точек во всех строчках изображения:' + ' ' + sum;
       }
       break;
     default:
-      return "hello" + b;
+      return 'hello' + b;
   }
 }
