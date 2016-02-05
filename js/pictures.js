7cd599f5992704151f45a4415276b49b85c26e37
pkozelskiy <pkozelskiy@n-core.ru>
2016-02-05 17:21:20 +0300

    [module4-task1] Выполнил задание

--- js/pictures.js
+++ js/pictures.js
@@ -1,16 +1,20 @@
 'use strict';
 
 (function() {
+
+  document.querySelector('.filters').classList.add('hidden');
+
   var container = document.querySelector('.pictures');
 
   pictures.forEach(function(picture) {
+    //Заполняем шаблон данными из полученного массива
     var element = createTemplate(picture);
-    console.log(element);
-
+    container.appendChild(element);
   });
 
+  //Создаем шаблон
   function createTemplate(data) {
-    
+
     var template = document.getElementById('picture-template');
 
     if ('content' in template) {
@@ -19,14 +23,25 @@
       var element = template.childNodes[0].cloneNode(true);
     }
 
-    var pictureNew = new Image();
+    var pictureNew = new Image(182,182);
+    pictureNew.title = data.date;
     var pictureOld = element.querySelector('.picture img');
-    element.replaceChild(pictureOld, pictureNew);
-    element.querySelector('.picture-stat .picture-comments').textContent(data.comments);
-    element.querySelector('.picture-stat .picture-likes').textContent(data.likes);
+    
+    pictureNew.onload = function() {
+      element.replaceChild(pictureNew, pictureOld);
+    }
+    pictureNew.onerror = function(e) {
+      element.replaceChild(pictureNew, pictureOld);
+      pictureNew.parentElement.classList.add('picture-load-failure');
+    }
 
+    pictureNew.src = data.url;
+    element.querySelector('.picture-comments').textContent = data.comments;
+    element.querySelector('.picture-likes').textContent = data.likes;
 
     return element;
   }
+
+  document.querySelector('.filters').classList.remove('hidden');
 
 })();
