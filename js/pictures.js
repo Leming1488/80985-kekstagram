7926ffe7da65194c5d38c303b1ae9c6894c8f1f7
pkozelskiy <pkozelskiy@n-core.ru>
2016-02-05 21:16:36 +0300

    [module5-task1] добавил запрос xhr

--- js/pictures.js
+++ js/pictures.js
@@ -1,16 +1,48 @@
 'use strict';
 
 (function() {
+  var pictures = [];
+  var xhr = new XMLHttpRequest();
+    var container = document.querySelector('.pictures');
+
+  xhr.open('GET', 'http://o0.github.io/assets/json/pictures.json');
+
+  xhr.timeout = 30000;
+
+  xhr.onload = function(e) {
+    var data = e.target.response;
+    pictures = JSON.parse(data);
+    renderPhoto(pictures);
+    container.classList.remove('pictures-loading')
+  };
+
+  xhr.onprogress = function() {
+    container.classList.add('pictures-loading');
+  };
+
+  xhr.ontimeout = function() {
+    container.classList.add('pictures-failure');
+  };
+
+  xhr.onabort = function() {
+    container.classList.add('pictures-failure');
+  };
+
+  xhr.send();
+
+
 
   document.querySelector('.filters').classList.add('hidden');
 
-  var container = document.querySelector('.pictures');
 
-  pictures.forEach(function(picture) {
-    //Заполняем шаблон данными из полученного массива
-    var element = createTemplate(picture);
-    container.appendChild(element);
-  });
+  function renderPhoto(pictures) {
+    pictures.forEach(function(picture) {
+      //Заполняем шаблон данными из полученного массива
+      var element = createTemplate(picture);
+      container.appendChild(element);
+    });
+  }
+
 
   //Создаем шаблон
   function createTemplate(data) {
@@ -26,11 +58,11 @@
     var pictureNew = new Image(182,182);
     pictureNew.title = data.date;
     var pictureOld = element.querySelector('.picture img');
-    
+
     pictureNew.onload = function() {
       element.replaceChild(pictureNew, pictureOld);
     }
-    pictureNew.onerror = function(e) {
+    pictureNew.onerror = function() {
       element.replaceChild(pictureNew, pictureOld);
       pictureNew.parentElement.classList.add('picture-load-failure');
     }
