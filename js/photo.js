c88d7abcd9517c7c51efe52d549d9ebe23f9f2ee
Leming1488 <borovweb@gmail.com>
2016-02-21 20:56:33 +0300

    [module7-task1] Добавил конструктор Photo

---
+++ js/photo.js
@@ -0,0 +1,17 @@
+  'use strict';
+
+  (function() {
+    /**
+    * @constructor
+    * @param {string} data
+    */
+  var Photo = function(data) {
+    this._data = data;
+  };
+
+  Resizer.prototype = {
+    render: function() {
+
+    }
+  };
+  })();
