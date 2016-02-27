1ce9620f612a9069a21c86cb55cdf73a933684fe
Leming1488 <borovweb@gmail.com>
2016-02-27 16:48:39 +0300

    [module8-task1] написал функцию

---
+++ js/inherit.js
@@ -0,0 +1,11 @@
+ 'use strict';
+ (function() {
+
+   function inherit(Child, Parent) {
+     function Empty() {}
+     Empty.prototype = Parent.prototype;
+     Child.prototype = new Empty();
+   }
+
+   window.inherit = inherit;
+ })();
